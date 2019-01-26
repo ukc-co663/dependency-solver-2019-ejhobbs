0a53c38d5262e10c1ce50f94eed3508740901f6a
Edward Hobbs <contact@ehobbs.it>
2019-01-26 14:35:37 +0000

    add processing for working out dependencies

--- repo_package.c
+++ repo_package.c
@@ -1,29 +1,72 @@
 #include "repo_package.h"
 
-package* package_fromJson(cJSON* jsonPkg){
+package* package_fromJson(const cJSON* jsonPkg){
   cJSON* name = cJSON_GetObjectItemCaseSensitive(jsonPkg, "name");
   cJSON* size = cJSON_GetObjectItemCaseSensitive(jsonPkg, "size");
   cJSON* version = cJSON_GetObjectItemCaseSensitive(jsonPkg, "version");
-  if (!cJSON_IsNumber(size) || !cJSON_IsString(name) || !cJSON_IsString(version)) {
+  cJSON* depends = cJSON_GetObjectItemCaseSensitive(jsonPkg, "depends");
+
+  /* Check everything's in order */
+  if (!cJSON_IsNumber(size) || !cJSON_IsString(name) ||
+      !cJSON_IsString(version) || !cJSON_IsArray(depends)) {
     fprintf(stderr, "Malformed package information\n");
     return NULL;
   }
+  int numDepends = cJSON_GetArraySize(depends);
 
   package* parsed = malloc(sizeof *parsed);
-
   parsed->name = name->valuestring;
   parsed->size = size->valueint;
   parsed->version = versionFromString(version->valuestring);
+  parsed->cDepends = numDepends;
+  parsed->depends = getAllDependencies(depends, numDepends);
   return parsed;
 }
 
-void package_prettyPrint(package* p) {
-  printf("-------------------\n");
-  printf("Name: %s\n", p->name);
-  printf("Size: %d\n", p->size);
-  printf("Version: %d.%d.%d\n", p->version[MAJOR], p->version[MINOR], p->version[PATCH]);
-  printf("-------------------\n");
+dependency** getAllDependencies(const cJSON* depList, int count) {
+  dependency** dependencies = calloc(count, sizeof(dependency*));
+  dependency** thisDep = dependencies;
+  const cJSON* dep = NULL;
+  cJSON_ArrayForEach(dep, depList) {
+    *thisDep = getDependency(dep->valuestring);
+    thisDep++;
+  }
+  return dependencies;
 }
+
+dependency* getDependency(char* dep) {
+  dependency* newDep = calloc(1, sizeof(dependency));
+  int sName = 0; /* Size of pkg name */
+  int relation = 0; /* Start with any */
+  char* curChar = dep;
+  /* Loop over the string until we get to the first number, ie the version starts */
+  while(curChar < dep + strlen(dep) && (*curChar > 0x39 || *curChar < 0x30)){
+    switch(*curChar) {
+      case '<':
+        relation |= _lt;
+        break;
+      case '>':
+        relation |= _gt;
+        break;
+      case '=':
+        relation |= _eq;
+        break;
+      default:
+        sName++;
+        break;
+    }
+    curChar++;
+  }
+  char* name = calloc(sName+1, sizeof(char));
+  strncpy(name, dep, sName);
+  name[sName] = 0;
+
+  newDep->name = name;
+  newDep->version = versionFromString(curChar);
+  newDep->comp = relation;
+  return newDep;
+}
+
 
 int* versionFromString(char* versionString) {
   int* v = calloc(_numversions, sizeof(int));
@@ -40,5 +83,24 @@ int* versionFromString(char* versionString) {
 
 void package_free(package* p) {
   free(p->version);
+  for (int i = 0; i < p->cDepends; i++) {
+    free(p->depends[i]->name);
+    free(p->depends[i]);
+  }
+  free(p->depends);
   free(p);
+}
+
+void package_prettyPrint(const package* p) {
+  printf("\n--[ %s ]--\n", p->name);
+  printf("Size: %d\n", p->size);
+  printf("Depends:\n");
+  for (int i=0; i < p->cDepends; i++) {
+    printf("\t %s ", p->depends[i]->name);
+    printf(": %d.%d.%d\n"
+        , p->depends[i]->version[MAJOR]
+        , p->depends[i]->version[MINOR]
+        , p->depends[i]->version[PATCH]);
+  }
+  printf("Version: %d.%d.%d\n", p->version[MAJOR], p->version[MINOR], p->version[PATCH]);
 }
--- repo_package.h
+++ repo_package.h
@@ -8,14 +8,29 @@
 #define MINOR 1
 #define PATCH 2
 
+/* Relation flags */
+#define _gt 1
+#define _lt 2
+#define _eq 4
+
+
+typedef struct dependency {
+  char* name;
+  int* version;
+  int comp;
+} dependency;
+
 typedef struct package {
   char* name;
   int size;
   int* version;
+  int cDepends;
+  dependency** depends;
 } package;
 
-package* package_fromJson(cJSON*);
+package* package_fromJson(const cJSON*);
 int* versionFromString(char*);
-void package_prettyPrint(package*);
-
+dependency** getAllDependencies(const cJSON*, int);
+dependency* getDependency(char*);
+void package_prettyPrint(const package*);
 void package_free(package*);

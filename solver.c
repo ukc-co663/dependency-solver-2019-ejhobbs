d7c06b816dd40fc1f8cb8e03056828574bfe9386
Edward Hobbs <eh362@kent.ac.uk>
2019-02-18 13:33:32 +0000

    skeleton for solving the problem

---
+++ solver.c
@@ -0,0 +1,10 @@
+#include "solver.h"
+
+int solver_newStateFromConstraints(repository* repo, constraints* instr, states* initial, states* out){
+  //TODO: implement
+  return 0;
+}
+
+constraints solver_toNewState(states* in, states* out){
+  return (constraints){0, NULL, NULL};
+}
---
+++ solver.h
@@ -0,0 +1,6 @@
+#include "repository.h"
+#include "state.h"
+#include "constraints.h"
+
+constraints solver_toNewState(states*, states*);
+int solver_newStateFromConstraints(repository*, constraints*, states*, states*);

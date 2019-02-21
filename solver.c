d6b9ce1e0a70321a6dc835baa2f4f75965c41403
Edward Hobbs <eh362@kent.ac.uk>
2019-02-21 22:11:32 +0000

    add rules for conflicts. also try to tidy up since there's a lot of duplicate code

--- solver.c
+++ solver.c
@@ -10,6 +10,20 @@ unsigned char charToOp(char* op) {
   return 0;
 }
 
+bool_exp* newExpression(char inst, package* pkg) {
+  bool_exp* exp = calloc(1, sizeof(*exp));
+  exp->option = inst;
+  exp->pkg = pkg;
+  return exp;
+}
+
+constraint_list* constraintFromDependency(char op, relation* pkg) {
+  constraint c = {op, *pkg};
+  constraint_list* cl = calloc(1, sizeof(*cl));
+  cl->cons = c;
+  return cl;
+}
+
 bool_exp_list* solver_getRules(repository* repo, constraint_list* cs) {
   bool_exp_list* rules = NULL;
   while(cs != NULL) {
@@ -18,44 +32,37 @@ bool_exp_list* solver_getRules(repository* repo, constraint_list* cs) {
     package* cPkg = repo->packages[idx];
 
     /* Create rule for this package */
-    bool_exp *thisPkg = calloc(1, sizeof(*thisPkg));
-    thisPkg->option = charToOp(&c.op);
-    thisPkg->pkg = cPkg;
     bool_exp_list* newRules = calloc(1, sizeof(*newRules));
-    newRules->exp = thisPkg;
+    newRules->exp = newExpression(charToOp(&c.op), cPkg);
     newRules->next = rules;
     rules = newRules;
 
+    /* new constraints created by adding dependencies */
+    constraint_list* head = NULL;
+    constraint_list* current = head;
     switch (c.op) {
       case '-':
-        /* don't need to add any of the dependencies, just create new rule for this */
+        /* nothing to do, we won't try uninstall things just because we don't need them */
         break;
       case '+':
         /* add each dep as +constraint */
         for (int i = 0; i < cPkg->cDepends; i++) {
-          relation_group* thisRGrp = &cPkg->depends[i];
+          relation_group* deps = &cPkg->depends[i];
           bool_exp* thisGrp = NULL;
           /* create A v B v C v D statement */
-          constraint_list* head = NULL;
-          constraint_list* current = head;
-          for (int j = 0; j < thisRGrp->size; j++) {
-            int idx = repo_getPackageIndex(repo, &thisRGrp->relations[j]);
+          for (int j = 0; j < deps->size; j++) {
+            /* repo, current, head -> return new current*/
+            int idx = repo_getPackageIndex(repo, &deps->relations[j]);
             package* thisPkg = repo->packages[idx];
-            if (thisPkg->seen != 1) {
-              thisPkg->seen = 1;
+            if (!(thisPkg->seen & KEEP)) {
+              thisPkg->seen &= KEEP;
 
-              bool_exp* newExp = calloc(1, sizeof(*newExp));
-              newExp->option = KEEP;
-              newExp->pkg = thisPkg;
+              bool_exp* newExp = newExpression(KEEP, thisPkg);
               newExp->next = thisGrp;
-
               thisGrp = newExp;
 
               /* Create new constraint to deal with dependencies of this dependency */
-              relation rel = {thisPkg->name, thisPkg->version, _eq};
-              constraint newC = {C_INSTALL, rel};
-              constraint_list* thisCons = calloc(1, sizeof(*thisCons));
-              thisCons->cons = newC;
+              constraint_list* thisCons = constraintFromDependency(C_INSTALL, &deps->relations[j]);
               if(head == NULL) {
                 head = thisCons;
               }
@@ -66,19 +73,41 @@ bool_exp_list* solver_getRules(repository* repo, constraint_list* cs) {
             }
           }
 
-          if (head != NULL) {
-            /* we've got some dependencies to add to the queue */
-            current->next = cs->next;
-            cs->next = head;
-          }
-
           bool_exp_list* newRules = calloc(1, sizeof(*newRules));
           newRules->exp = thisGrp;
           newRules->next = rules;
           rules = newRules;
         }
         /* add each conflict as -constraint*/
+        for (int i = 0; i < cPkg->cConflicts; i++) {
+          int idx = repo_getPackageIndex(repo, &cPkg->conflicts[i]);
+          package* thisConflict = repo->packages[idx];
+          if(!(thisConflict->seen & REMOVE)) {
+            thisConflict->seen &= REMOVE;
+
+            bool_exp_list* newRule = calloc(1, sizeof(*newRule));
+            newRule->exp = newExpression(REMOVE, thisConflict);
+            newRule->next = rules;
+            rules = newRule;
+
+            constraint_list* thisCons = constraintFromDependency(C_REMOVE, &cPkg->conflicts[i]);
+
+            if (head == NULL) {
+              head = thisCons;
+            }
+            if(current != NULL) {
+              current->next = thisCons;
+            }
+            current = thisCons;
+          }
+        }
         break;
+    }
+
+    if (head != NULL) {
+      /* we've got some dependencies to add to the queue */
+      current->next = cs->next;
+      cs->next = head;
     }
     cs = cs->next;
   }

5715169b1a3da8576772245f1fdb3074a8863a3f
Edward Hobbs <contact@ehobbs.it>
2019-02-21 11:50:37 +0000

    taking a new approach with solving, based on SAT-solver algorithm from OPIUM and 0install

--- solver.c
+++ solver.c
@@ -1,19 +1,1 @@
 #include "solver.h"
-int installPackage(repository*, package*, states*, states*);
-int uninstallPackage(repository*, package*, states*, states*);
-
-int solver_newStateFromConstraints(repository* repo, constraints* instr, states* in, states* out){
-  return 0;
-}
-
-int installPackage(repository* repo, package* pkg, states* in, states* out) {
-  return 0;
-}
-
-int uninstallPackage(repository* repo, package* pkg, states* in, states* out){
-  return 0;
-}
-
-constraints solver_toNewState(states* in, states* out){
-  return (constraints){0, NULL, NULL};
-}
--- solver.h
+++ solver.h
@@ -2,5 +2,25 @@
 #include "state.h"
 #include "constraints.h"
 
-constraints solver_toNewState(states*, states*);
-int solver_newStateFromConstraints(repository*, constraints*, states*, states*);
+typedef struct bool_exp {
+  unsigned char option;
+  package* pkg;
+} bool_exp;
+
+typedef struct bool_exp_list {
+  bool_exp exp;
+  bool_exp* next;
+}
+
+typedef struct pkg_rule {
+  package* owner;
+  unsigned int expr_count;
+  bool_exp_list expressions;
+}
+
+typedef struct pkg_rule_list {
+  pkg_rule rule;
+  pkg_rule* next;
+}
+
+pkg_rule_list get_final_state(repository* repo, constraints* cs);
